d1fa2954bad502b54eab73c59dfe3bbb111a336c
jakub-wroblewski <92018407+jakub-wroblewski@users.noreply.github.com>
2022-04-07 16:21:55 +0200

    a

---
+++ untitled8/zadania.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+
+#Zadanie1
+# a = np.array([[1],[2],[3]])
+# b = np.array([[4, 5, 6 ]])
+# wynik = np.dot(a,b)
+# print(a)
+# print(b)
+# print(wynik)
+
+
+#Zadanie2
+# x = np.array([[2, -3, 10],[2,99,-12],[0, 53, -1]])
+# y = np.array([[52, -7, 102],[21,9,-2],[10, 23, -15]])
+#
+# print(x.min(axis = 0))
+# print(x.min(axis = 1))
+#
+# print(y.min(axis = 0))
+# print(y.min(axis = 1))
+
+
+#Zadanie3
+
+# print(a.dot(b))
+
+
+
+
+#Zadanie4
+
+
+b=np.random.random_integers(1,9,(1,3))
+a=np.random.random_integers(1,9,(1,3))
+
+print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
